d49b2412a6754976ad2c62c1f9835720ae3d4b5a
Mac <>
2025-05-27 19:01:34 +0900

    로그인 구현중

--- Assets/Script/UI/Rank/UIMyRankItem.cs
+++ Assets/Script/UI/Rank/UIMyRankItem.cs
@@ -12,14 +12,24 @@ public class UIMyRankItem : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public Image medalImage;
     public Sprite[] medalSprites;
+    public GameObject lockPanel;
     private int _index;
     
     public void Set()
     {
         nameText.text = Main.Ins.MainUser.GetName();
-        scoreText.text = Main.Ins.MainUser.GetScore().ToString();
-        playCountText.text = Main.Ins.MainUser.GetPlayCount().ToString();
         
+        var score = Main.Ins.MainUser.GetScore();
+        if (score <= 0)
+        {
+            lockPanel.gameObject.SetActive(true);
+        }
+        else
+        {
+            scoreText.text = Main.Ins.MainUser.GetScore().ToString();
+            playCountText.text = Main.Ins.MainUser.GetPlayCount().ToString();
+            lockPanel.gameObject.SetActive(false);
+        }
         var rank = Main.Ins.MainUser.GetRank();
         rankText.text = rank == -1 ? "-" : rank.ToString();
     }
