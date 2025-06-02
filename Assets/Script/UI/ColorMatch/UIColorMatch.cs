4b142b73347ad856cb09c2beb693b9591900ba0e
Mus340 <musness74@gmail.com>
2025-06-02 12:17:18 +0900

    스코어 표기 방식 변경 중

--- Assets/Script/UI/ColorMatch/UIColorMatch.cs
+++ Assets/Script/UI/ColorMatch/UIColorMatch.cs
@@ -10,17 +10,29 @@ using UnityEngine.UI;
 public class UIColorMatch : UIContentPanel
 {
     public Text scoreText;
-    public Text maxScoreText;
     public Text timerText;
 
-    public GameObject sliderPanel;
-    public Slider scoreSlider;
     private Tween _scoreTween;
     
     private int _prevScore;
+
+    public GameObject posPanel;
+    public GameObject curScorePos;
+    public GameObject maxScorePos;
+    public UIRankPos uIRankPosOrigin;
+    private List<UIRankPos> _rankPosList;
     
     protected override void Initialize()
     {
+        _rankPosList = new();
+        for (int i = 0; i < Main.Ins.MainData.RankData.RankList.Count; i++)
+        {
+            var item = Instantiate(uIRankPosOrigin, uIRankPosOrigin.transform.parent);
+            item.transform.SetAsFirstSibling();
+            item.Set(i);
+            _rankPosList.Add(item);
+        }
+        uIRankPosOrigin.gameObject.SetActive(false);
     }
 
     protected override void Enter()
@@ -28,14 +40,14 @@ public class UIColorMatch : UIContentPanel
         var game = Main.Ins.MainGame.GameContentProvider.GetGameContent<ColorMatchContent>(GameType.ColorMatch);
         _prevScore = game.Score;
         scoreText.text = $"{game.Score}";
-        maxScoreText.text = $"{game.MaxScore}";
-        scoreSlider.maxValue = game.MaxScore;
-        scoreSlider.value = game.Score;
-
-        sliderPanel.SetActive(game.MaxScore > 0);
+        
+        maxScorePos.SetActive(game.MaxScore > 0);
+        
+        SetPos();
         game.OnNext.Subscribe((_) =>
         {
-            ShowScoreEffect(game.Score, _prevScore);
+            SetPos();
+            ShowScoreEffect(game.Score,_prevScore);
             _prevScore = game.Score;
 
         }).AddTo(Disposable);
@@ -48,7 +60,32 @@ public class UIColorMatch : UIContentPanel
             })
             .AddTo(this);
     }
+    
+    private void SetPos()
+    {
+        var content = Main.Ins.MainGame.GameContentProvider.GetGameContent<ColorMatchContent>(GameType.ColorMatch);
+        var curScore = content.Score;
+        var maxScore = content.MaxScore;
 
+        float max = Mathf.Max(curScore, maxScore, Main.Ins.MainData.RankData.RankList[0].MaxScore, 1f);
+        float panelWidth = ((RectTransform)posPanel.transform).rect.width;
+
+        SetXPositionSmooth(curScorePos, curScore, max, panelWidth);
+        SetXPositionSmooth(maxScorePos, maxScore, max, panelWidth);
+
+        for (int i = 0; i < Main.Ins.MainData.RankData.RankList.Count; i++)
+        {
+            SetXPositionSmooth(_rankPosList[i].gameObject, Main.Ins.MainData.RankData.RankList[i].MaxScore, max, panelWidth);
+        }
+    }
+    
+    private void SetXPositionSmooth(GameObject obj, float score, float maxScore,float panelWidth)
+    {
+        var normalized = Mathf.Clamp01(score / maxScore);
+        var rt = obj.GetComponent<RectTransform>();
+        rt.DOAnchorPos(new Vector2(normalized * panelWidth, 0), 0.3f)
+            .SetEase(Ease.OutQuad);
+    }
     private void ShowScoreEffect(int curScore, int prevScore)
     {
         _scoreTween?.Kill();
@@ -59,7 +96,6 @@ public class UIColorMatch : UIContentPanel
             onVirtualUpdate: value =>
             {
                 scoreText.text = value.ToString("N0");
-                scoreSlider.value = value;
             }
         );
     }
---
+++ Assets/Script/UI/ColorMatch/UIRankPos.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRankPos : MonoBehaviour
+{
+    public Image image;
+    public Text text;
+    
+    private int _rank;
+
+    public void Set(int rank)
+    {
+        _rank = rank;
+        text.text = _rank.ToString();
+        Color color;
+        if (_rank == 0)
+        {
+            ColorUtility.TryParseHtmlString("#FFFF00", out color);
+        }
+        else if (_rank == 1)
+        {
+            ColorUtility.TryParseHtmlString("#ABABAB", out color);
+        }
+        else if (_rank == 2)
+        {
+            ColorUtility.TryParseHtmlString("#FF6700", out color);
+        }
+        else
+        {
+            ColorUtility.TryParseHtmlString("#FFFFFF", out color);
+        }
+        image.color = color;
+    }
+}
